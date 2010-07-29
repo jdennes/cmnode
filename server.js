1af4a6bb4115c50658aab61d49b55aaf79b13cc4
James Dennes <jdennes@gmail.com>
2010-07-29 23:05:08 +1000

    Fixed api polling.

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ var sys = require('sys'),
   http = require('http'),
   ws = require('./lib/ws'),
   xml2js = require('./lib/xml2js'),
+  campaign = null,
   opens = 0,
   uniqueOpens = 0,
   clicks = 0,
   unsubscribes = 0,
-  bounces = 0;
+  bounces = 0,
+  output_queue = [];
 
 try {
   var configJSON = fs.readFileSync(__dirname + "/config/app.json");
@@ -15,8 +17,6 @@ try {
   sys.log("File config/app.json not found. Try: `cp config/app.json.example config/app.json`");
 }
 var config = JSON.parse(configJSON.toString());
-
-var campaign = null;
 
 function getCampaign() {
   // Finds the campaign from a list of the client's campaigns using 
@@ -46,12 +46,11 @@ function getCampaign() {
     });
   });
 }
-  
+
 function processCMResponse(websocket, xml) {
   var parser = new xml2js.Parser();
   parser.addListener('end', function(result) {
-    sys.debug(sys.inspect(result));
-    var output = '<p>';
+    var output = '';
     var _opens = result.TotalOpened;
     var _uniqueOpens = result.UniqueOpened;
     var _clicks = result.Clicks;
@@ -59,11 +58,11 @@ function processCMResponse(websocket, xml) {
     var _bounces = result.Bounced;
     if (_opens && _uniqueOpens && _clicks && _unsubscribes && _bounces) {
       if (opens != _opens) {
-        output += (_opens - opens) + ' new open' + ((_opens - opens) != 1 ? 's' : '') + ' ';
+        output += (_opens - opens) + ' new open' + ((_opens - opens) != 1 ? 's' : '') + '; ';
         opens = _opens;
       }
       if (uniqueOpens != _uniqueOpens) {
-        output += (_uniqueOpens - uniqueOpens) + ' new unique open' + ((_uniqueOpens - uniqueOpens) != 1 ? 's' : '') + ' ';
+        output += (_uniqueOpens - uniqueOpens) + ' new unique open' + ((_uniqueOpens - uniqueOpens) != 1 ? 's' : '') + '; ';
         uniqueOpens = _uniqueOpens;
       }
       if (clicks != _clicks) {
@@ -71,43 +70,40 @@ function processCMResponse(websocket, xml) {
         clicks = _clicks;
       }
       if (unsubscribes != _unsubscribes) {
-        output += (_unsubscribes - unsubscribes) + ' new unsubscribe' + ((_unsubscribes - unsubscribes) != 1 ? 's' : '') + ' ';
+        output += (_unsubscribes - unsubscribes) + ' new unsubscribe' + ((_unsubscribes - unsubscribes) != 1 ? 's' : '') + '; ';
         unsubscribes = _unsubscribes;
       }
       if (bounces != _bounces) {
-        output += (_bounces - bounces) + ' new bounce' + ((_bounces - bounces) != 1 ? 's' : '') + ' ';
+        output += (_bounces - bounces) + ' new bounce' + ((_bounces - bounces) != 1 ? 's' : '') + '; ';
         bounces = _bounces;
       }
-      if (output === '<p>') {
-        output = '.'; 
-      } else {
-        output += '</p>';
-      }
     }
-    var jsonResponse = {
-      CampaignName: campaign.Name,
-      Opens: opens,
-      UniqueOpens: uniqueOpens,
-      Clicks: clicks,
-      Unsubscribes: unsubscribes,
-      Bounces: bounces,
-      New: output
-    };
-    sys.debug(JSON.stringify(jsonResponse));
-    websocket.write(JSON.stringify(jsonResponse));
+    if (output != '') {
+      var jsonResponse = {
+        CampaignName: campaign.Name,
+        CampaignSubject: campaign.Subject,
+        Opens: opens,
+        UniqueOpens: uniqueOpens,
+        Clicks: clicks,
+        Unsubscribes: unsubscribes,
+        Bounces: bounces,
+        Message: output
+      };
+      sys.debug(JSON.stringify(jsonResponse));
+      websocket.write(JSON.stringify(jsonResponse));
+    }
   });
   parser.parseString(xml); 
 }
 
 ws.createServer(function(websocket) {
-  if (!campaign) { getCampaign(); }
-
-  websocket.addListener('connect', function(response) {
-    sys.debug('connect: ' + response);
-
-    // Server polls Campaign Monitor API for a campaign's summary
-    setInterval(function() {
-      var data = '';
+  if (!campaign) {
+    getCampaign();
+  }
+  // Server polls Campaign Monitor API for a campaign's summary
+  setInterval(function() {
+    var data = '';
+    try {
       var cm = http.createClient(80, 'api.createsend.com');
       var request = cm.request('GET', '/api/api.asmx/Campaign.GetSummary?ApiKey=' + 
         config.api_key + '&CampaignID=' + config.campaign_id, {'host': 'api.createsend.com'});
@@ -118,16 +114,20 @@ ws.createServer(function(websocket) {
           data += chunk;
         });
         response.addListener('end', function() {
-          //sys.debug('Response body: \n' + data);
           processCMResponse(websocket, data);
         });
       });
-    }, 2000);
-
+    } catch (e) {
+      // If we can't make the call, just wait until we can
+      sys.log("Failed to connect to Campaign Monitor API.");
+    }
+  }, 2000);
+  
+  websocket.addListener('connect', function(response) {
+    sys.debug('connect: ' + response);
   }).addListener('data', function(data) {
     sys.debug(data);
   }).addListener('close', function() {
     sys.debug('close');
-    
   });
 }).listen(8000);
