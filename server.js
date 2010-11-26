565843eb3bde46346809031034dd7606b0fd99d0
James Dennes <jdennes@gmail.com>
2010-11-26 11:54:32 +1100

    Using node-geoip and google maps api now.

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var sys = require('sys'),
   fs = require('fs'),
   http = require('http'),
   ws = require('./lib/ws'),
+  geoip = require('./lib/geoip'),
+  dbpath = '/usr/local/share/GeoIP/GeoLiteCity.dat'
   campaign = null,
   opens = {};
 
@@ -37,29 +39,34 @@ function getCampaign() {
 function processCMResponse(websocket, data) {
   // Grab the most recent 100 opens
   var _raw_opens = JSON.parse(data).Results;
-  var _new_opens = {};
+  var _new_opens = null;
 
-  _raw_opens.forEach(function(o) {
-    key = o.EmailAddress + o.Date;
-    if (!(key in opens)) {
-      opens[key] = o;
-      _new_opens[key] = o;
-    }
-  });
+  if (_raw_opens) {
+    _raw_opens.forEach(function(o) {
+      key = o.EmailAddress + o.Date;
+      if (!(key in opens)) {
+        var con = new geoip.Connection(dbpath, function(con) {
+          con.query(o.IPAddress, function(result) {
+            o.latitude = result.latitude;
+            o.longitude = result.longitude;
+            con.close();
+          });
+        });
+        opens[key] = o;
+        if (!_new_opens) { _new_opens = {}; }
+        _new_opens[key] = o;
+      }
+    });
   
-  var output = '';
-  for (var k in _new_opens) {
-    v = _new_opens[k];
-    output += 'Open by ' + v.EmailAddress + ' at ' + v.Date + ' (ip: ' + v.IPAddress + ')<br />';
-  }
-
-  if (output != '') {
-    var jsonResponse = {
-      CampaignName: campaign.Name,
-      CampaignSubject: campaign.Subject,
-      Message: output
-    };
-    websocket.write(JSON.stringify(jsonResponse));
+    if (_new_opens) {
+      var jsonResponse = {
+        CampaignName: campaign.Name,
+        CampaignSubject: campaign.Subject,
+        NewOpens: _new_opens
+      };
+      console.log(jsonResponse);
+      websocket.write(JSON.stringify(jsonResponse));
+    }
   }
 }
 
@@ -68,10 +75,12 @@ ws.createServer(function(websocket) {
     getCampaign();
   }
 
-  // Server polls Campaign Monitor API for a campaign's summary
+  // Server polls Campaign Monitor API for campaign opens
   setInterval(function() {
     var data = '';
     try {
+      var now = new Date();
+      var mindate = now.getUTCFullYear() + '-' + (now.getUTCMonth() + 1) + '-' + now.getUTCDate();
       var cm = http.createClient(80, 'api.createsend.com');
       var request = cm.request('GET', '/api/v3/campaigns/' + config.campaign_id + '/opens.json?date=2010-01-01&pagesize=100&orderfield=date&orderdirection=desc',
         { 'host': 'api.createsend.com', 'Authorization': 'Basic ' + new Buffer(config.api_key + ':x').toString('base64') });
